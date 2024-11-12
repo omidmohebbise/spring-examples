6d348fe9617d2e3e5c380d2237f8071e5700a505
omidmohebbi <omidmohebbise@gmail.com>
2024-11-12 13:17:48 +0100

    Spring-security: add spring-oauth-example

---
+++ spring-security/spring-oauth-client/src/test/java/com/omidmohebbise/springoauthclient/SpringOauthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omidmohebbise.springoauthclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
