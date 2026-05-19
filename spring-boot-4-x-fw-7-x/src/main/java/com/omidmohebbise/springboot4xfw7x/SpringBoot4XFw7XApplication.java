7219307c706f081df0f6c2e981f7f48b6be5b0bd
Omid.Mohebbi <omid.mohebbi@rabobank.nl>
2026-05-19 09:28:43 +0200

    Initial spring boot 4x project

---
+++ spring-boot-4-x-fw-7-x/src/main/java/com/omidmohebbise/springboot4xfw7x/SpringBoot4XFw7XApplication.java
@@ -0,0 +1,13 @@
+package com.omidmohebbise.springboot4xfw7x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4XFw7XApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot4XFw7XApplication.class, args);
+    }
+
+}
---
+++ spring-boot-4-x-fw-7-x/src/test/java/com/omidmohebbise/springboot4xfw7x/SpringBoot4XFw7XApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omidmohebbise.springboot4xfw7x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot4XFw7XApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
